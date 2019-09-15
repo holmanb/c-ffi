3d99dc9dbbf010a03bf1e40e025ab36a686d846c
Brett Holman <bpholman5@gmail.com>
2019-09-14 20:50:47 -0600

    use compiler flag (rpath) to specify the directory for the shared object

--- go/main.go
+++ go/main.go
@@ -2,7 +2,7 @@ package main
 
 /*
 #cgo CFLAGS: -I ./set-c
-#cgo LDFLAGS: -L ./set-c
+#cgo LDFLAGS: -L ./set-c  -Lset-c -lset
 
 #include "stdlib.h"
 #include "set.h"
@@ -18,21 +18,18 @@ import (
 func main() {
 	i := 1
 	j := 1.0
-//	k := C.CString("c")
+	//k := C.CString("c")
 	k := byte(99)
 
-	fmt.Printf("%c : %T \n", k,k)
+	//fmt.Printf("%c : %T \n", k,k)
 
 	p := C.set_init()
 	defer C.set_free(p)
 	fmt.Println("set initiated ")
-
 	C.set_add(p, unsafe.Pointer(&i), C.INT);
 	C.set_add(p, unsafe.Pointer(&j), C.DOUBLE);
 	C.set_add(p, unsafe.Pointer(&k), C.CHAR);
-
 	C.set_print(p)
-	fmt.Println("pointer %v",p)
 }
 
 
